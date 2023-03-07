6972460f1f463c7f4259019021dc59c2d3179ef5
Gogolbog <111005996+Gogolbog@users.noreply.github.com>
2023-03-07 13:38:51 +0200

    c

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,31 +1,39 @@
+import PropTypes from "prop-types";
 
-
-export const Profile = () => {
-    return (<div class="profile">
-        <div class="description">
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+    return (<div className="profile">
+        <div className="description">
             <img
-                src="..."
+                src={avatar}
                 alt="User avatar"
-                class="avatar"
+                className="avatar"
             />
-            <p class="name">Petra Marica</p>
-            <p class="tag">@pmarica</p>
-            <p class="location">Salvador, Brasil</p>
+            <p className="name">{username}</p>
+            <p className="tag">@{tag}</p>
+            <p className="location">{location}</p>
         </div>
 
-        <ul class="stats">
+        <ul className="stats">
             <li>
-                <span class="label">Followers</span>
-                <span class="quantity">1000</span>
+                <span className="label">Followers</span>
+                <span className="quantity">{stats.followers}</span>
             </li>
             <li>
-                <span class="label">Views</span>
-                <span class="quantity">2000</span>
+                <span className="label">Views</span>
+                <span className="quantity">{stats.views}</span>
             </li>
             <li>
-                <span class="label">Likes</span>
-                <span class="quantity">3000</span>
+                <span className="label">Likes</span>
+                <span className="quantity">{stats.likes}</span>
             </li>
         </ul>
     </div>)
 };
+
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.objectOf(PropTypes.number),
+}
